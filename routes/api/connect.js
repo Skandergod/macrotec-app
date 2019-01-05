2c00eb244a8b38315e1c0442abccef8c49f1a156
Ivan <guerrero.ivan.789@gmail.com>
2019-01-04 22:33:34 -0500

    middleware assets y software autodesk creado

--- routes/api/connect.js
+++ routes/api/connect.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     host     : 'businet-web.com',
-    user     : 'fsanmcbx_macrotecApp',
-    password : 'fsanmcbx_macrote',
-    database : 'appcontraseña'
+    database     : 'fsanmcbx_macrotecApp',
+    user : 'fsanmcbx_macrote',
+    password : 'appcontraseña'
   });
 
   module.exports = connection;
