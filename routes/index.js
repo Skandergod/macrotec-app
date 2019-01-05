dc24d93fe955a6832aa672698b660e969d6f4c8c
Ivan <guerrero.ivan.789@gmail.com>
2019-01-04 19:02:23 -0500

    Dashboard agregado

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'El webo mio' });
+  res.render('index', { title: 'Macrotec App' });
+});
+router.get('/home', function(req, res, next) {
+  res.render('dashboard/home', { title: 'Home' });
 });
 
 module.exports = router;
