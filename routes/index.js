791a87d43aad65b6364e7ded3c2231ec22698938
Ivan <guerrero.ivan.789@gmail.com>
2019-01-07 18:41:29 -0500

    redirect if logged

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,25 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport')
   , LocalStrategy = require('passport-local').Strategy;
-
+function loggedIn(req, res, next) {
+    if (req.user) {
+        next();
+    } else {
+        res.redirect('/');
+    }
+}
+function loggedInHome(req, res, next) {
+  if (req.user) {
+    res.redirect('/home');
+  } else {
+    next();
+  }
+}
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/',loggedInHome, function(req, res, next) {
   res.render('index', { title: 'Macrotec App' });
 });
-router.get('/home', function(req, res, next) {
+router.get('/home',loggedIn, function(req, res, next) {
   res.render('dashboard/home', { title: 'Home' });
 });
 router.get('/administracion/softwares-autodesk', function(req, res, next) {
